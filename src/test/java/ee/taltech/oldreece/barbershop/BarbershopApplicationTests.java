735a74a568aa2acbe577b166c4ed38665023aa53
mhasan <mhasan@ttu.ee>
2019-05-27 12:42:51 +0300

    Initial commit

---
+++ src/test/java/ee/taltech/oldreece/barbershop/BarbershopApplicationTests.java
@@ -0,0 +1,16 @@
+package ee.taltech.oldreece.barbershop;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BarbershopApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}

c792dd0856285df4489bf6b558844186fbe9f8b4
mhasan <mhasan@ttu.ee>
2019-06-01 18:20:39 +0300

    Stylist Controller and Service added

--- src/main/java/ee/taltech/oldreece/barbershop/service/StylistService.java
+++ src/main/java/ee/taltech/oldreece/barbershop/service/StylistService.java
@@ -1,9 +1,36 @@
 package ee.taltech.oldreece.barbershop.service;
 
+import ee.taltech.oldreece.barbershop.model.Stylist;
+import ee.taltech.oldreece.barbershop.repository.StylistRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class StylistService {
+    @Autowired
+    private StylistRepo stylistRepo;
+
+    public List<Stylist> getAllStylist() {
+        return stylistRepo.findAll();
+    }
+
+    public boolean updateAStylist(Stylist stylist) {
+        stylistRepo.save(stylist);
+        return true;
+    }
+
+    public boolean addAStylist(Stylist stylist) {
+        stylist.setId(null);
+        stylistRepo.save(stylist);
+        return true;
+    }
+
+    public boolean deleteAStylistByID(Long id) {
+        stylistRepo.deleteById(id);
+        return true;
+    }
 
 }
 

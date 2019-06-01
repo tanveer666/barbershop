8437df8cbb7c64dc77d8949a79c4ed76d6056e7b
zekena <zekena@taltech.ee>
2019-06-01 23:43:43 +0300

    controller test

--- src/test/java/ee/taltech/oldreece/barbershop/controllerTests/DateControllerTest.java
+++ src/test/java/ee/taltech/oldreece/barbershop/controllerTests/DateControllerTest.java
@@ -1,4 +1,37 @@
 package ee.taltech.oldreece.barbershop.controllerTests;
 
+import ee.taltech.oldreece.barbershop.model.ReservationDate;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class DateControllerTest {
+
+    private final ParameterizedTypeReference<List<ReservationDate>> LIST_OF_DATES = new ParameterizedTypeReference<List<ReservationDate>>() {
+
+    };
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+    @Test
+    public void returns_all_dates() {
+        ResponseEntity<List<ReservationDate>> exchange= testRestTemplate.exchange("/date", HttpMethod.GET,null,LIST_OF_DATES);
+        assertEquals(HttpStatus.OK,exchange.getStatusCode());
+        List<ReservationDate> dates = exchange.getBody();
+        assertNotNull(dates);
+        assertEquals(dates.size(),4);
+    }
 }

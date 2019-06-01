fe0ff9bad5f285f865e17a63f98839a75459f319
mhasan <mhasan@ttu.ee>
2019-06-02 00:37:54 +0300

    index page added

--- src/main/java/ee/taltech/oldreece/barbershop/controllers/IndexController.java
+++ src/main/java/ee/taltech/oldreece/barbershop/controllers/IndexController.java
@@ -1,14 +1,15 @@
 package ee.taltech.oldreece.barbershop.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("/")
 public class IndexController {
     @GetMapping
     public String greeting() {
-        return "Welcome to Reece's Hair and Facial Studio";
+        return "index.html";
     }
 }
